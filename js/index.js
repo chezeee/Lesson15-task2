ae3c66d0827b2d17fe0abeb99958a72cb36a339f
Ilya Yalymov <chezeee@gmail.com>
2023-02-05 22:16:20 +0300

    Initial structure added

---
+++ js/index.js
@@ -0,0 +1,14 @@
+class Tasks {
+  /**
+   * @param {string[]} todos
+   */
+  constructor(todos) {
+    this._todos = todos;
+  }
+
+  // your code
+}
+
+// Sample usage - do not modify
+const tasks = new Tasks(["Laundry", "Clean kitchen"]);
+tasks.csvString; // "Laundry, Clean kitchen"
